d9c8d7843f6a69ba7fba03deda2ea7ef4876441b
Kathrine <kathrine.kodehode@gmail.com>
2023-01-10 21:09:34 +0100

    update

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,14 +1,12 @@
 import React from "react";
+import Xmas from "../images/neonxmasTrim.mp4";
 
 export default function Background() {
   return (
     <section className="Wrap">
       {" "}
       <video className="vidBack" autoPlay muted loop>
-        <source
-          src="https://doc-10-3c-docs.googleusercontent.com/docs/securesc/upaq8u9qu2bhuiobpg424vm96gi7apu0/t2k1437d5iu3smelcl2cft3qda9glghm/1673361450000/05725122105321560004/15876361969160272701/1fPXY9Zaf4bWv7oyc3I7GTRdvPhBYUFk6?ax=ALjR8syU4srUYsAGR0bJWINTinhY9N9vkQiZ8FuMm2NutgB5OrfUD7vVjQomD4q5iTEdElOPKRzBnDRAYZsa58DzsMYXrirIMF0DTvLxpk3uZPZ0kBvLTRPXrNFg-9Ad6YPix2GQf9U32VSHtCpcYkEG__5flVD1dCPoR5ivcKO_qVogPFB97-Swxd87PFecj2oxSUz7b2N4HveGBpr9VUY9m0FMSh3OrmonqxOY0DcJJVZHzUXFEs5q-6F5vg4KFJHqGD_N_7DUzqsHyHCFB9bh14CpwxxVJ81xfVtwGFVhDjWVPcdiJSE4_Jr5IenJRR588prbQpBc-Zn7xpu4KGILV-8qFGykVZ8JcrlJOf8MrsrredcEt69NMrmJ_6CmyXjsb7JdyzyMf-FX4H2PfCqtmwgA5pkPGjDiWMkxUYO2Tt6oLh4DN8qJqEGZcOHFFYS6YvdISh7498xu5hhPN5q025eg1eiuhGk2SyopFusm4f66tjXLTiW29BEuB6fCG-E-WKMZcV8M22aIvE_f9SrjhneGBCAAvLx2U7NvPT1ZzpP6Oi2WjHgqXyl-Wu3nLHgoACHbFq0kFoRpbKyaFcgEQW5_bGcaSr9X3ah5orTcJZxIfOvorvLH9VWOXwugQK1kjISbz-4Uj3FLtLL-C2cOvh4fVV8e1FXSEX80TRYaw7TrqA5czm7TmrOGuq4Z7yp-WSou-lEWsSX26yFcV3E9dFwUd-ak4XmM3Nk1ue4vEyaci0tzBsi-y6TwLyst-Dp9jc-0Lb-Q3BETeT1jO5iUtdmXKVE9R0yTWkTDlKTp5uKPV9OSK7HsWDBi3ikqIlwAHtSf00t8jDUcYSX4O7n2QuW_QC0Cvmp3OtzErhcPvQXlgmwVT4W4SyKLwgQjoHYT7F9szggLfP2GNlMaW0WL6VOY1qTbRgNyHa4&uuid=b490905b-eb63-4447-a376-f3ab104fcc13&authuser=0&nonce=kicaedpgqe3c6&user=15876361969160272701&hash=974ibc2nabjl7aknfq1qlhkv3ar79e0j"
-          type="video/mp4"
-        ></source>
+        <source src={Xmas} type="video/mp4"></source>
       </video>
     </section>
   );
